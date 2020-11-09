6aa01bb48d26c28e777d634d8555ecec179b4649
Jura L <jural@Juras-MBP.lan>
2020-11-09 10:57:23 +0200

    Fix button import

--- src/components/LoadingButton.js
+++ src/components/LoadingButton.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 import { ImSpinner2 } from 'react-icons/im';
 import './LoadingButton.css';
 
