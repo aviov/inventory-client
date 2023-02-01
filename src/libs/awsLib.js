cdd71cd43e3104cd2b47397a932c54def3d50ef7
Jura L <jural@Juras-MBP.lan>
2023-02-02 00:02:36 +0200

    Add shared bucket * add public upload * resolves issue #33 * parallel commit on back end

--- src/libs/awsLib.js
+++ src/libs/awsLib.js
@@ -2,14 +2,15 @@ import { Storage } from 'aws-amplify';
 
 export async function s3Upload(file) {
   const filename = `${Date.now()}-${file.name}`;
-  const stored = await Storage.vault.put(filename, file, {
-    contentType: file.type
+  const stored = await Storage.put(filename, file, {
+    contentType: file.type,
+    level: 'public'
   });
   return stored.key;
 };
 
 export async function s3Delete(filename) {
-  await Storage.vault.remove(filename);
+  await Storage.remove(filename);
   return filename;
   // const removed = await Storage.vault.remove(filename);
   // console.log('removed', removed);
@@ -17,6 +18,6 @@ export async function s3Delete(filename) {
 };
 
 export async function s3FileURL(filename) {
-  const fileURL = await Storage.vault.get(filename);
+  const fileURL = await Storage.get(filename);
   return fileURL;
 };
