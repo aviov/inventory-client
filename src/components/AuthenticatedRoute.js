958e0ef2dca5322069e9b402af7658ce5a60eb8e
Juri <juri.l@me.com>
2023-06-08 22:48:27 +0300

    Update react (#60)
    * Update react
    * Update react
    * Fix vulnerabilities
    * Update dependancies
    * Migration fix router
    * Migration fix bootstrap
    * Clean code
    * Rename component Routes to Pages
    * Resolves issue #55
    
    * Fix update react
    * Fix route components
    * Resolves issue #55
    
    ---------
    
    Co-authored-by: Jura L <jural@Juras-MBP.lan>

--- src/components/AuthenticatedRoute.js
+++ src/components/AuthenticatedRoute.js
@@ -1,30 +1,17 @@
 import React from 'react';
-import { Route, Navigate, useLocation } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { useAuthContext } from '../libs/contextLib';
 
 export default function AuthenticatedRoute({ children, redirectTo }) {
   const { pathname, search } = useLocation();
   const { isAuthenticated } = useAuthContext();
 
-  // const AuthenticatedElement = isAuthenticated ? (
-  //   <React.Fragment>
-  //     {element}
-  //   </React.Fragment>
-  // ) : (
-  //   <Navigate
-  //     to={`/login?redirect=${pathname}${search}`}
-  //   />
-  // );
-
-  // return(
-  //   <Route {...rest} element={<AuthenticatedElement />} />
-  // );
   return(isAuthenticated ? (
     children
   ) : (
     <Navigate
-      to={'/'}
-      // to={`/login?redirect=${pathname}${search}`}
+      // to={'/'}
+      to={`/login?redirect=${pathname}${search}`}
     />
   ));
 }
